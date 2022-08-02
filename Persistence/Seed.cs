b62784ee99dcc124193007fc6c8fb3ac9c8c11a7
yll <yllshillova@gmail.com>
2022-08-02 09:04:17 -0700

    End of Section 3

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -11,7 +11,7 @@ namespace Persistence
         public static async Task SeedData(DataContext context)
         {
             if (context.Activities.Any()) return;
-            
+
             var activities = new List<Activity>
             {
                 new Activity
